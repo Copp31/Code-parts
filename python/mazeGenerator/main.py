bcfe908d5c4e4709a09ac73ab2f922582b33cdcf
Copp3lia <86722908+Copp31@users.noreply.github.com>
2022-08-04 14:51:46 -0400

    Update and rename python/main.py to python/mazeGenerator/main.py

--- python/mazeGenerator/main.py
+++ python/mazeGenerator/main.py
@@ -1,15 +1,5 @@
 from labyrinthe import Labyrinthe
 
-"""
-Voici mon projet labyrinthe. Il fonctionne et produit un graph très bien. 
-
-La grosse lacune de mon projet est la section test... j'y ai laissé un commentaire. 
-
-Forte inspiration du projet : https://scipython.com/blog/making-a-maze/
-
-    Coppélia
-
-"""
 
 # Initialisation du nombre de colonnes et de rangées
 largeur: int = 8
